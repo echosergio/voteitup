f1a7a734b2988d29b85fffde86adbd19484bdf4f
Sergio Vicente de las Heras <sergio.v.he@gmail.com>
2017-11-24 19:39:10 +0100

    Add splash screen and login activity

---
+++ App/VoteItUp/app/src/main/java/upm/dam/voteitup/SplashActivity.kt
@@ -0,0 +1,17 @@
+package upm.dam.voteitup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
