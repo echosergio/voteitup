8673e8700e0066e0bd8a113737dc0b0e36b29e35
Sergio Vicente de las Heras <sergio.v.he@gmail.com>
2017-11-25 13:35:14 +0100

    Create login screen

--- App/VoteItUp/app/src/main/java/upm/dam/voteitup/SplashActivity.kt
+++ App/VoteItUp/app/src/main/java/upm/dam/voteitup/SplashActivity.kt
@@ -9,7 +9,7 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(2000)
+        Thread.sleep(1000)
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
