f5fafb45f1c2f24ed1d9c3405ef52743d0cd1883
gonzo <gonza.cerro@gmail.com>
2017-12-17 13:37:50 +0100

    unitard with layouts!

--- App/VoteItUp/app/src/main/java/upm/dam/voteitup/activities/CreatePollActivity.kt
+++ App/VoteItUp/app/src/main/java/upm/dam/voteitup/activities/CreatePollActivity.kt
@@ -2,12 +2,53 @@ package upm.dam.voteitup.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_create_poll.*
+import kotlinx.android.synthetic.main.activity_main.*
 import upm.dam.voteitup.R
+import android.widget.AdapterView.OnItemClickListener
+import kotlinx.android.synthetic.main.list_view_answers.view.*
+import upm.dam.voteitup.R.string.Example_Answer
+import upm.dam.voteitup.adapters.AnswerListAdapter
+
 
 class CreatePollActivity : AppCompatActivity() {
+
+    private lateinit var answersAdapter: AnswerListAdapter
+
+    private lateinit var list: MutableList<EditText>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_poll)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        var edittxt = EditText(this)
+        edittxt.hint = Example_Answer.toString()
+        list = mutableListOf( edittxt )
+        answersAdapter = AnswerListAdapter(baseContext,
+                listData = list)
+
+
+        val listView = findViewById<ListView>(R.id.answerList)
+        listView.adapter = answersAdapter
+
+        plusAnswer.setOnClickListener { addNewAnswer() }
+        answerList.onItemClickListener = AdapterView.OnItemClickListener { a, v, position, id ->
+            val item = answersAdapter.getItem(position)
+            answersAdapter.notifyDataSetChanged()
+        }
+
+    }
+
+    private fun addNewAnswer() {
+        //val listView = findViewById<ListView>(R.id.answerList)
+        var edittxt = EditText(this)
+        edittxt.hint = Example_Answer.toString()
+        list.add(edittxt)
+        answersAdapter.notifyDataSetChanged()
+
     }
 }
---
+++ App/VoteItUp/app/src/main/java/upm/dam/voteitup/adapters/AnswerListAdapter.kt
@@ -0,0 +1,66 @@
+package upm.dam.voteitup.adapters
+
+import android.content.Context
+import android.support.v4.content.res.ResourcesCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import upm.dam.voteitup.R
+import upm.dam.voteitup.entities.Poll
+
+class AnswerListAdapter(private val context: Context, private val listData: MutableList<EditText>) : BaseAdapter() {
+    private val layoutInflater: LayoutInflater
+
+    init {
+        layoutInflater = LayoutInflater.from(context)
+    }
+
+    override fun getCount(): Int {
+        return listData.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return listData[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var convertView = convertView
+        val holder: ViewHolder
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.list_view_answers, null)
+
+            holder = ViewHolder()
+            holder.editAnswerView = convertView!!.findViewById<EditText>(R.id.editText1)
+            convertView!!.setTag(holder)
+        } else {
+            holder = convertView!!.getTag() as ViewHolder
+        }
+
+        val answer = this.listData[position].text
+        holder.editAnswerView!!.setText(answer)
+
+        holder.editAnswerView!!.onFocusChangeListener = View.OnFocusChangeListener{ view, hasFocus ->
+            if (!hasFocus) {
+                if (!holder.editAnswerView!!.isInEditMode) {
+                    var text = holder.editAnswerView!!.text
+                    this.listData[position].
+                            setText(text.toString(),TextView.BufferType.EDITABLE)
+                }
+            }
+        }
+
+        return convertView
+    }
+
+    internal class ViewHolder {
+        var editAnswerView: EditText? = null
+    }
+}
